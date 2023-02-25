f33687cb1e63666ef84dd9885f7594df4f10619f
Andrew <47224786+afulle12@users.noreply.github.com>
2023-02-24 22:31:47 -0700

    Add files via upload

---
+++ c-like.rs
@@ -0,0 +1,25 @@
+// An attribute to hide warnings for unused code.
+#![allow(dead_code)]
+
+// enum with implicit discriminator (starts at 0)
+enum Number {
+    Zero,
+    One,
+    Two,
+}
+
+// enum with explicit discriminator
+enum Color {
+    Red = 0xff0000,
+    Green = 0x00ff00,
+    Blue = 0x0000ff,
+}
+
+fn main() {
+    // `enums` can be cast as integers.
+    println!("Two is {}", Number::Two as i32);
+    println!("one is {}", Number::One as i32);
+
+    println!("roses are #{:06x}", Color::Red as i32);
+    println!("violets are #{:06x}", Color::Green as i32);
+}
